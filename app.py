43266484a35652886cbf5cfa7381e7aa75c63e17
Redfabian3310 <fabiancch-wm22@student.tarc.edu.my>
2025-08-23 13:26:24 +0800

    update

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ if uploaded_file:
         fill_color="rgba(0, 0, 0, 0)",
         stroke_width=3,
         stroke_color="#00FF00",
-        background_image=Image.fromarray(display_img),
+        background_image=Image.fromarray(display_img.astype("uint8")),
         update_streamlit=True,
         height=display_img.shape[0],
         width=display_img.shape[1],
